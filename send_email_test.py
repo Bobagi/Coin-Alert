e990f336f9ada9ee4608e1b2d9cecaeba21a026d
Gustavo Perin <gustavoperin067@gmail.com>
2024-02-18 19:40:53 -0300

    Added send_email function with the delete Alerts method

--- send_email_test.py
+++ send_email_test.py
@@ -1,7 +1,6 @@
 import os
 import smtplib
 import time
-import requests
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from dotenv import load_dotenv
@@ -24,16 +23,7 @@ def send_email(subject, body, to_email):
     text = msg.as_string()
     server.sendmail(from_email, to_email, text)
     server.quit()
-
-def clear_alerts():
-    url = "https://bobagi.net/api/cryptoAlert/clearAlerts"
-    try:
-        response = requests.post(url)
-        response.raise_for_status()
-        print("Alerts cleared successfully!")
-    except requests.exceptions.RequestException as e:
-        print(f"Error while clearing alerts: {e}")
-
+    
 while True:
     subject = "Test Email from coin-watcher"
     body = "This is a test email."
@@ -41,6 +31,4 @@ while True:
     send_email(subject, body, to_email)
     print("Email sent successfully!")
 
-    clear_alerts()
-
-    time.sleep(600)  # Sleep for 10 minutes
+    time.sleep(600)  # Sleep for 5 seconds
